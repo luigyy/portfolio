df6ec946784c27e54ef4333f4e85efd21e8ebe3b
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-25 17:10:25 -0600

    finish hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import {
+  AiOutlineInstagram,
+  AiFillGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 interface HeroProps {}
 
 const Hero: React.FC<HeroProps> = ({}) => {
   return (
-    <div>
-      <img src="pic-profile.jpg" className="w-16 rounded-full px-12" alt="Me" />
+    <div className="px-12 w-full">
+      <img src="./pic-profile.jpg" className="w-16 rounded-full  " alt="Me" />
+      <div className="max-w-2xl mt-10">
+        <h1 className="text-5xl text-zinc-800 font-bold tracking-tight">
+          Coding enthusiast, developer and always a student!
+        </h1>
+        <p className="text-zinc-600 mt-10">
+          Hi I am Luigy, a software engineer from San José, Costa Rica. I've
+          been playing with code for as long as I can remember. Lately, I've
+          been interested in web development, where I have both worked as
+          freelancer and built plenty of projects.
+        </p>
+      </div>
+      <span className="mt-10 flex gap-7">
+        <button className="text-zinc-600 text-3xl ">
+          <AiOutlineInstagram />
+        </button>
+        <button className="text-zinc-600 text-3xl">
+          <AiFillGithub />
+        </button>
+        <button className="text-zinc-600 text-3xl">
+          <AiFillLinkedin />
+        </button>
+      </span>
     </div>
   );
 };
--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { FiSun } from "react-icons/fi";
+import { useState } from "react";
 
 interface ToggleThemeProps {}
 
 const ToggleTheme: React.FC<ToggleThemeProps> = ({}) => {
+  const [theme, setTheme] = useState("lofi");
+  const toggleTheme = () => {
+    setTheme(theme === "night" ? "lofi" : "night");
+  };
+  // initially set the theme and "listen" for changes to apply them to the HTML tag
+  React.useEffect(() => {
+    document.querySelector("html")!.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
-    <div className=" backdrop-blur mt-6 absolute right-[80px] px-3 py-2 shadow-md ring-1 ring-zinc-900/5 rounded-full">
-      <FiSun className="text-xl opacity-60 hover:text-emerald-600" />
+    <div className=" backdrop-blur mt-6 absolute right-[80px]  shadow-md ring-1 ring-zinc-900/5 rounded-full">
+      <button
+        // onClick={() => toggleTheme()}
+        className="items-center text-xl opacity-60 hover:text-emerald-600 px-3 py-2"
+      >
+        <FiSun className="" />
+      </button>
     </div>
   );
 };
