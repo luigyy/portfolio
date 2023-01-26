53a1db0c72a82c00eeb450e076ec554a28739cd4
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-25 21:42:24 -0600

    added blob to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ interface HeroProps {}
 
 const Hero: React.FC<HeroProps> = ({}) => {
   return (
-    <div className="px-12 w-full">
+    <div className="px-12 w-full relative">
       <img src="./pic-profile.jpg" className="w-16 rounded-full  " alt="Me" />
       <div className="max-w-2xl mt-10">
         <h1 className="text-5xl text-zinc-800 font-bold tracking-tight">
@@ -33,6 +33,13 @@ const Hero: React.FC<HeroProps> = ({}) => {
           <AiFillLinkedin />
         </button>
       </span>
+
+      {/* animated spheres  */}
+      <div>
+        <div className="w-60 h-60 bg-emerald-500 absolute right-28 top-14 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000"></div>
+        <div className="w-60 h-60 bg-red-500 absolute right-10 top-14 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-1000"></div>
+        <div className="w-60 h-60 bg-yellow-500 absolute right-4 top-20 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-4000"></div>
+      </div>
     </div>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 15s infinite",
+      },
+
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(-150px, 50px) scale(1.2)",
+          },
+          "66%": {
+            transform: "translate(75px, -100px) scale(0.8)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
