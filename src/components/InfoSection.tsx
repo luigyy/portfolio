8dfdf7f698773bfa73d5c3dc6ef14cfb86686b0f
luigyy <luigy.valverde@ucr.ac.cr>
2023-11-22 14:58:23 -0600

    added experience section

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { TfiDownload } from "react-icons/tfi";
-import { MdEmail } from "react-icons/md";
 import { ImBooks } from "react-icons/im";
+import { MdWorkOutline } from "react-icons/md";
 
 const EMAIL = "luigy.valverde@ucr.ac.cr";
 interface InfoSectionProps {}
@@ -19,43 +19,41 @@ const InfoSection: React.FC<InfoSectionProps> = ({}) => {
       {" "}
       {/* right div  */}
       <div className="flex flex-col gap-16 ">
-        {/* email div  */}
-        <div
-          onMouseLeave={() => setCopied(false)}
-          className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40 bg-blue-50/10"
-        >
-          <h1 className="text-sm font-semibold text-zinc-800 flex items-center gap-2">
-            {" "}
-            <MdEmail className=" text-zinc-400 text-2xl" />{" "}
-            <span className="text-zinc-800">Email me</span>
-          </h1>
-          <p className="text-sm text-zinc-500 mt-2">
-            Feel free to reach out anytime through any of my socials or via
-            email.
-          </p>
-          <div className="">
-            <input
-              type="text"
-              disabled
-              value={EMAIL}
-              className="
-              text-zinc-500 text-sm
-              w-[60%] mx-auto disabled:bg-white  ring-1 ring-zinc-900/5  shadow-md appearance-none rounded-lg mt-5 px-3 py-2"
-            />
-            <button
-              onClick={() => copyToClipboard()}
-              onMouseEnter={() => setCopied(false)}
-              className="ml-3 text-white bg-emerald-600 py-2 px-3 rounded-lg"
-            >
-              Copy
-            </button>
+        <div className=" rounded-2xl border border-zinc-100 p-6 ">
+          <div className="flex items-center gap-3 ">
+            <span className="text-2xl text-zinc-500">
+              <MdWorkOutline />
+            </span>
+            <span className="text-sm text-zinc-800 font-semibold ">
+              Experience
+            </span>
           </div>
-          <p className="text-sm text-emerald-500 mt-4 text-left">
-            {copied ? "Copied to clipboard!" : ""}
-          </p>
+          {/* education div  */}
+          <div className="" id="education">
+            {/* ucr  */}
+            <div className="mt-5 flex gap-4">
+              <div className="p-1 overflow-hidden h-10 w-10 flex items-center justify-center rounded-full shadow-md shadow-zinc-500/5 ring-1 ring-zinc-900/5">
+                <img
+                  loading="lazy"
+                  src="movi_logo_min.png"
+                  className="bg-transparent object-contain rounded-full h-7 w-7"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex justify-between w-full">
+                  <p className="text-zinc-700 font-semibold text-sm">Movi</p>
+                  <p className="text-zinc-700  italic text-sm">
+                    05/2023 - Present
+                  </p>
+                </div>
+                <div className=" flex w-full mt-2 text-[12px]">
+                  <p className="text-zinc-600 pr-10">Jr full-stack developer</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        {/* email div  */}
         <div className=" rounded-2xl border border-zinc-100 p-6 ">
           <div className="flex items-center gap-3 ">
             <span className="text-2xl text-zinc-500">
